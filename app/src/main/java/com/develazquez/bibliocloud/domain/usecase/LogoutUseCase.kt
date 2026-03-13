5deb20e43df6f6df6a5b61352a45f90ceccec39a
rocioballinasvallejo <221230@ids.upchiapas.edu.mx>
2026-03-13 16:36:43 -0600

    Implementación de lógica de Logout y botón en Catálogo

---
+++ app/src/main/java/com/develazquez/bibliocloud/domain/usecase/LogoutUseCase.kt
@@ -0,0 +1,12 @@
+package com.develazquez.bibliocloud.domain.usecase
+
+import com.develazquez.bibliocloud.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class LogoutUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(): Result<Unit> {
+        return authRepository.logout()
+    }
+}
